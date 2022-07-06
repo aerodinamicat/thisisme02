5fc7a89961798f25e02512fa2659d2b7bc045e65
aerodinamicat <aerodinamicat@gmail.com>
2022-07-06 12:57:21 +0200

    Implemented user and person models

--- models/person.go
+++ models/person.go
@@ -1,1 +1,16 @@
 package models
+
+import "database/sql"
+
+type Person struct {
+	FirstName     string       `json:"firstName"`
+	SecondName    string       `json:"secondName"`
+	FirstSurname  string       `json:"firstSurname"`
+	SecondSurname string       `json:"secondSurname"`
+	Gender        string       `json:"gender"`
+	BirthDate     sql.NullTime `json:"birthDate"`
+
+	UserId    string       `json:"userId"`
+	CreatedAt sql.NullTime `json:"createdAt"`
+	UpdatedAt sql.NullTime `json:"updatedAt"`
+}
--- models/user.go
+++ models/user.go
@@ -1,1 +1,12 @@
 package models
+
+import "database/sql"
+
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+
+	CreatedAt sql.NullTime `json:"createdAt"`
+	UpdateAt  sql.NullTime `json:"updatedAt"`
+}
